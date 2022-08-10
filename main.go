0bf46bd9074a0a81e8f92dae879e29144a1cf8f9
MacBobby Chibuzor <theghostmac@gmail.com>
2022-08-11 00:20:00 +0100

    bootstrapped the client UI

--- main.go
+++ main.go
@@ -14,6 +14,22 @@ func main() {
 	mux.Handle("/static", http.StripPrefix("/static/", files))
 	// redirect the root url to the handler function
 	mux.HandleFunc("/", index)
+	// other necessary functions to handle
+	mux.HandleFunc("/err", err)
+
+	// authentication functions
+	mux.HandleFunc("/login", login)
+	mux.HandleFunc("/logout", logout)
+	mux.HandleFunc("/signup", signup)
+	mux.HandleFunc("signup_account", signupAccount)
+	mux.HandleFunc("/authenticate", authenticate)
+
+	// threads and posts functions
+	mux.HandleFunc("thread/new", newThread)
+	mux.HandleFunc("thread/create", createThread)
+	mux.HandleFunc("thread/post", postThread)
+	mux.HandleFunc("threa/read", readThread)
+
 	// web server logic
 	server := &http.Server{
 		Addr:    "0.0.0.0:8080",
@@ -22,6 +38,7 @@ func main() {
 	server.ListenAndServe()
 }
 
+// index handler function generates the HTML and write to the ResponseWriter
 func index(writer http.ResponseWriter, request *http.Request) {
 	files := []string{
 		"templates/layout.html",
