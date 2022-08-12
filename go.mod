9c875604b50fbab41ada6636f25ecb93fdf64781
jim-nnamdi <jimsamuel50@gmail.com>
2022-08-13 00:02:56 +0100

    add uuid to user struct & installed pkg

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.17
 
 require github.com/lib/pq v1.10.6
 
-require github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/google/uuid v1.3.0 // indirect
+)
--- routeAuth.go
+++ routeAuth.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/golang-jwt/jwt"
+	"github.com/google/uuid"
 )
 
 func authenticate(writer http.ResponseWriter, request *http.Request) {
@@ -12,18 +13,37 @@ func authenticate(writer http.ResponseWriter, request *http.Request) {
 	// user, _ := data.U
 }
 
+// to create a token you will need some data to be
+// encoded inside the generated jwt token
+// so that you will be able to call the fields from the claims
+
 type DataToEncode struct {
 	Username string `json:"username"`
 	Email    string `json:"email"`
 	jwt.StandardClaims
 }
 
+type User struct {
+	ID       uuid.UUID `json:"id"`
+	Username string    `json:"username"`
+	Password string    `json:"password"`
+	Email    string    `json:"email"`
+}
+
 func AuthUser() {
+
+	var (
+		user_data = User{}
+	)
 
 	// let's say you've authenticated the user
 	// we want to generate cookies, but we need a token
 	// preferably jwt tokens
 
 	expiration_date := time.Now().Add(5 * time.Minute)
+
+	encode_data := DataToEncode{
+		Username: user_data.Username,
+	}
 
 }
