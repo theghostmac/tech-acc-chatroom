61b15d0d00f733cd9adb5d2b3c79124b0889be73
jim-nnamdi <jimsamuel50@gmail.com>
2022-08-12 23:59:03 +0100

    review on uuid (start token & cookie generation)

--- routeAuth.go
+++ routeAuth.go
@@ -2,11 +2,28 @@ package main
 
 import (
 	"net/http"
+	"time"
 
-	"github.com/theghostmac/tech-acc-chatroom/data"
+	"github.com/golang-jwt/jwt"
 )
 
 func authenticate(writer http.ResponseWriter, request *http.Request) {
 	request.ParseForm()
-	user, _ := data.U
+	// user, _ := data.U
+}
+
+type DataToEncode struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	jwt.StandardClaims
+}
+
+func AuthUser() {
+
+	// let's say you've authenticated the user
+	// we want to generate cookies, but we need a token
+	// preferably jwt tokens
+
+	expiration_date := time.Now().Add(5 * time.Minute)
+
 }
